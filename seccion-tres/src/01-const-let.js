67b7b549896ff60d599c4e21f6492218f4a4b488
Eduardo <Epadrinoa@gmail.com>
2022-12-05 13:26:36 -0400

    const-let
    creación de proyecto clase de constantes y let

---
+++ seccion-tres/src/01-const-let.js
@@ -0,0 +1,23 @@
+// Variables y Constantes
+
+const nombre = "Eduardo";
+const apellido = "Padrino";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido);
+
+// no se usa mas la variable de tipo ( var )
+
+if (true) {
+  // dentro de un scout pueden existir variables repetidas
+
+  const nombre = "Enrique";
+  console.log(nombre);
+
+  const valorDado = 8;
+  console.log(valorDado);
+}
+
+console.log(valorDado);
