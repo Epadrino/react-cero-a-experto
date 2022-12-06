8d0ef327df44a5f0e9ea879505b1e2e60cb6a211
Eduardo <Epadrinoa@gmail.com>
2022-12-06 13:12:41 -0400

    tarea
    incorporación de dos botones mas uno que resta y uno que coloca la prop default

--- seccion_dos_counter_vite/src/CounterApp.jsx
+++ seccion_dos_counter_vite/src/CounterApp.jsx
@@ -8,12 +8,22 @@ export const CounterApp = ({ value }) => {
     setCounter(counter + 1);
     //setCounter((c) => c + 1);
   };
+  const handleNeg = () => {
+    setCounter(counter - 1);
+    //setCounter((c) => c - 1);
+  };
+  const handleReset = () => {
+    setCounter(value);
+    //setCounter((c) => value);
+  };
 
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{counter}</h2>
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleNeg}>-1</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 };
