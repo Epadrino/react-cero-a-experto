b44691137317af330b80d4655b623032484ef5c3
Eduardo <Epadrinoa@gmail.com>
2022-12-06 11:12:13 -0400

    evento_click
    se agrego un button

--- seccion_dos_counter_vite/src/CounterApp.jsx
+++ seccion_dos_counter_vite/src/CounterApp.jsx
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
 
+const handleAdd = (value) => {
+  console.log(value);
+};
+
 export const CounterApp = ({ value }) => {
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{value}</h2>
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
--- seccion_dos_counter_vite/src/HelloWorldApp.jsx
+++ seccion_dos_counter_vite/src/HelloWorldApp.jsx
@@ -1,5 +1,3 @@
-import { FirstApp } from "./FirstApp";
-
 export const HelloWorldApp = () => {
   return <h1>HelloWorldApp </h1>;
 };
