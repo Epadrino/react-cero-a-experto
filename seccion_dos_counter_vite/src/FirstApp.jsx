6f797b35b23e61ed8f90e55c13d913f8b7c455d1
Eduardo <Epadrinoa@gmail.com>
2022-12-07 17:37:15 -0400

    finalizacin_de_las_pruebas_en_first_app
    se le agrego el test para verificar que no falte el título

--- seccion_dos_counter_vite/src/FirstApp.jsx
+++ seccion_dos_counter_vite/src/FirstApp.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export const FirstApp = ({ title }) => {
   return (
     <>
-      <h1>{title}</h1>
+      <h1 data-testid="test-title">{title}</h1>
       <p>explicacion del nodo padre </p>
     </>
   );
--- seccion_dos_counter_vite/tests/FirstApp.test.jsx
+++ seccion_dos_counter_vite/tests/FirstApp.test.jsx
@@ -2,16 +2,19 @@ import { render } from "@testing-library/react";
 import { FirstApp } from "../src/FirstApp";
 
 describe("Pruebas en <FirstApp/>>", () => {
-  test("should hacer match con el snapshot", () => {
-    const title = "hola, Soy Goku";
-    const { container } = render(<FirstApp title={title} />);
-    expect(container).toMatchSnapshot();
-  });
+  // test("should hacer match con el snapshot", () => {
+  //   const title = "hola, Soy Goku";
+  //   const { container } = render(<FirstApp title={title} />);
+  //   expect(container).toMatchSnapshot();
+  // });
   test("should de mostrar el titulo en un h1", () => {
     const title = "hola, Soy Goku";
-    const { container, getByText } = render(<FirstApp title={title} />);
+    const { container, getByText, getByTestId } = render(
+      <FirstApp title={title} />
+    );
     expect(getByText(title)).toBeTruthy();
-    const h1 = container.querySelector("h1");
-    expect(h1.innerHTML).toContain(title);
+    // const h1 = container.querySelector("h1");
+    // expect(h1.innerHTML).toContain(title);
+    expect(getByTestId("test-title").innerHTML).toBe(title);
   });
 });
