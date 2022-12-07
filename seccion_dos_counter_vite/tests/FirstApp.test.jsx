e5dfbc6f2e20f496f2ce21e512aad6f24bcf1178
Eduardo <Epadrinoa@gmail.com>
2022-12-07 17:14:20 -0400

    probar_first_app
    se realizaron diversas pruebas y diferentes opciones

--- seccion_dos_counter_vite/tests/FirstApp.test.jsx
+++ seccion_dos_counter_vite/tests/FirstApp.test.jsx
@@ -3,6 +3,10 @@ import { FirstApp } from "../src/FirstApp";
 
 describe("Pruebas en <FirstApp/>>", () => {
   test("should hacer match con el snapshot", () => {
-    render(<FirstApp />);
+    const title = "hola, Soy Goku";
+    const { container, getByText } = render(<FirstApp title={title} />);
+    expect(getByText(title)).toBeTruthy();
+    const h1 = container.querySelector("h1");
+    expect(h1.innerHTML).toContain(title);
   });
 });
