36bfc388b39bf343f36ec029a126d45b27faac32
Eduardo <Epadrinoa@gmail.com>
2022-12-07 17:20:38 -0400

    se_agrego_el_snapshot
    se agrego la funcionalidad de foto al componente con el fin de comprar cambios o resguardo del diseño

--- seccion_dos_counter_vite/tests/FirstApp.test.jsx
+++ seccion_dos_counter_vite/tests/FirstApp.test.jsx
@@ -4,6 +4,11 @@ import { FirstApp } from "../src/FirstApp";
 describe("Pruebas en <FirstApp/>>", () => {
   test("should hacer match con el snapshot", () => {
     const title = "hola, Soy Goku";
+    const { container } = render(<FirstApp title={title} />);
+    expect(container).toMatchSnapshot();
+  });
+  test("should de mostrar el titulo en un h1", () => {
+    const title = "hola, Soy Goku";
     const { container, getByText } = render(<FirstApp title={title} />);
     expect(getByText(title)).toBeTruthy();
     const h1 = container.querySelector("h1");
