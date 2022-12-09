4aa579d36b0adef8e6cc0ba4bb7418bac6d550fa
Eduardo <Epadrinoa@gmail.com>
2022-12-09 09:50:40 -0400

    creando_una_lista_de_categoria
    implementacion del primer hook useState

--- gif_expert_app/src/GiftExpertApp.jsx
+++ gif_expert_app/src/GiftExpertApp.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+
 export const GiftExpertApp = () => {
-  return <div>GiftExpertApp</div>;
+  const [categories, setCategories] = useState([1, 2, 3, 4]);
+
+  return (
+    <>
+      {/* titulo */}
+      <h1>GiftExpertApp</h1>
+      {/* imput */}
+      {/* listado de Gif */}
+      {categories.map((category) => {
+        return <li key={category}>{category}</li>;
+      })}
+      {/* Gif Item */}
+    </>
+  );
 };
