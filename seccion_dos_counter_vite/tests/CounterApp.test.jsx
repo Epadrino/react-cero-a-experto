27d7752706c8a3e6d571e911983ff0c0aa95ce24
Eduardo <Epadrinoa@gmail.com>
2022-12-09 09:04:08 -0400

    test_button
    se realizaron pruebas de los botones de + , - y reset. se culmino la seccion de test

--- seccion_dos_counter_vite/tests/CounterApp.test.jsx
+++ seccion_dos_counter_vite/tests/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CounterApp } from "../src/CounterApp";
 
 describe("Pruebas en <CounterApp/>>", () => {
@@ -8,11 +8,35 @@ describe("Pruebas en <CounterApp/>>", () => {
     const { container } = render(<CounterApp value={value} />);
     expect(container).toMatchSnapshot();
   });
+
   test("should aparecer el mensaje '100'", () => {
     render(<CounterApp value={value} />);
     expect(screen.getByText(100)).toBeTruthy();
-    expect(screen.getByRole("heading", { level: 2 }).innerHTML).toContain(
-      "100"
-    );
+    // expect(screen.getByRole("heading", { level: 2 }).innerHTML).toContain("100");
+  });
+
+  test("should de incrementar con el boton +1", () => {
+    render(<CounterApp value={value} />);
+    fireEvent.click(screen.getByText("+1"));
+    // screen.debug();
+    expect(screen.getByText("101")).toBeTruthy();
+  });
+
+  test("should de decrementar con el boton -1", () => {
+    render(<CounterApp value={value} />);
+    fireEvent.click(screen.getByText("-1"));
+    // screen.debug();
+    expect(screen.getByText("99")).toBeTruthy();
+  });
+
+  test("should de funsionar el boton de reset", () => {
+    render(<CounterApp value={value} />);
+    fireEvent.click(screen.getByText("+1"));
+    fireEvent.click(screen.getByText("+1"));
+    fireEvent.click(screen.getByText("+1"));
+    //fireEvent.click(screen.getByText("Reset"));
+    fireEvent.click(screen.getByRole("button", { name: "btn-reset" }));
+
+    expect(screen.getByText(value)).toBeTruthy();
   });
 });
