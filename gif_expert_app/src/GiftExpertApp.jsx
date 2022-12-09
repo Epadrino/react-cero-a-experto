b7b6bbc292737449d39c45b9b0b5b055627ec413
Eduardo <Epadrinoa@gmail.com>
2022-12-09 11:54:01 -0400

    creacion_de_comp_input
    se creo el componente input tipo formulario

--- gif_expert_app/src/GiftExpertApp.jsx
+++ gif_expert_app/src/GiftExpertApp.jsx
@@ -1,25 +1,28 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GiftExpertApp = () => {
-  const [categories, setCategories] = useState(["One Punch", "dragon Ball"]);
+	const [categories, setCategories] = useState(['One Punch', 'dragon Ball']);
 
-  const onAddCategory = () => {
-    setCategories([...categories, "Valoran"]);
-    // setCategories = [...categories, "Valoran"];
-  };
-  return (
-    <>
-      {/* titulo */}
-      <h1>GiftExpertApp</h1>
-      {/* imput */}
-      {/* listado de Gif */}
-      <button onClick={onAddCategory}>Agregar</button>
-      <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
-      </ol>
-      {/* Gif Item */}
-    </>
-  );
+	const onAddCategory = () => {
+		setCategories([...categories, 'Valoran']);
+		// setCategories = [...categories, "Valoran"];
+	};
+	return (
+		<>
+			{/* titulo */}
+			<h1>GiftExpertApp</h1>
+
+			{/* imput */}
+			<AddCategory />
+			{/* listado de Gif */}
+			<button onClick={onAddCategory}>Agregar</button>
+			<ol>
+				{categories.map((category) => {
+					return <li key={category}>{category}</li>;
+				})}
+			</ol>
+			{/* Gif Item */}
+		</>
+	);
 };
