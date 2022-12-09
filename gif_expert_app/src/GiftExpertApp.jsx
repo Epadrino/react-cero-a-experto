4de2c3234c75fcd28c989bab6291932ee429cb32
Eduardo <Epadrinoa@gmail.com>
2022-12-09 10:19:32 -0400

    agragar_una_nueva_categoria

--- gif_expert_app/src/GiftExpertApp.jsx
+++ gif_expert_app/src/GiftExpertApp.jsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
 
 export const GiftExpertApp = () => {
-  const [categories, setCategories] = useState([1, 2, 3, 4]);
+  const [categories, setCategories] = useState(["One Punch", "dragon Ball"]);
 
+  const onAddCategory = () => {
+    setCategories([...categories, "Valoran"]);
+    // setCategories = [...categories, "Valoran"];
+  };
   return (
     <>
       {/* titulo */}
       <h1>GiftExpertApp</h1>
       {/* imput */}
       {/* listado de Gif */}
-      {categories.map((category) => {
-        return <li key={category}>{category}</li>;
-      })}
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
       {/* Gif Item */}
     </>
   );
