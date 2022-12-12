2100a72278e127353d2be0959a5626a68c978a54
Eduardo <Epadrinoa@gmail.com>
2022-12-12 10:10:24 -0400

    incorporacion_de_la_api

--- gif_expert_app/src/components/GifGrid.jsx
+++ gif_expert_app/src/components/GifGrid.jsx
@@ -1,3 +1,15 @@
+const getGifs = async (category) => {
+	const url = `api.giphy.com/v1/gifs/trending?api_key=dJZnWSm0oGKvjqM1ORfw7Gw9owKdBHlz&q=${category}`;
+	const resp = await fetch(url);
+	const { data } = await resp.json();
+	const gifs = data.map((img) => ({
+		id: img.id,
+		tittle: img.tittle,
+		url: img.images.downsized_medium.url,
+	}));
+	return gifs;
+};
+
 export const GifGrid = ({ category }) => {
 	return (
 		<>
