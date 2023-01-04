fe0cf5a306ac0b9f97b47e520470776968c5e380
Eduardo <Epadrinoa@gmail.com>
2023-01-04 10:36:07 -0400

    idea_general_de_un_reducer

---
+++ hook_app/src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,13 @@
+export const TodoApp = () => {
+	return (
+		<>
+			<h1>TodoApp</h1>
+			<hr />
+			<ul>
+				<li>Item1</li>
+				<li>Item2</li>
+				<li>Item3</li>
+			</ul>
+		</>
+	);
+};
--- hook_app/src/main.jsx
+++ hook_app/src/main.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { MainApp } from './09-useContext/MainApp';
+// import { BrowserRouter } from 'react-router-dom';
+// import { MainApp } from './09-useContext/MainApp';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { HooksApp } from './HooksApp';
-
+import './08-useReducer/intro-reducer';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<BrowserRouter>
-		<React.StrictMode>
-			<MainApp />
-		</React.StrictMode>
-	</BrowserRouter>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+// 	<BrowserRouter>
+// 		<React.StrictMode>
+// 			<MainApp />
+// 		</React.StrictMode>
+// 	</BrowserRouter>
+// );
