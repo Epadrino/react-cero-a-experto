fd4d07f74c5d4ad520851e5e6ac5fe2be9a0abdb
Eduardo <Epadrinoa@gmail.com>
2023-01-10 17:02:30 -0400

    conectando_ambos_hooks

--- hook_app/src/03-examples/MultiplesCustomHooks.jsx
+++ hook_app/src/03-examples/MultiplesCustomHooks.jsx
@@ -1,12 +1,16 @@
+import { useCounter } from '../hooks/useCounter';
 import { useFetch } from '../hooks/useFetch';
 
 export const MultiplesCustomHooks = () => {
 	// id necesario para encontrar el pokemon
 	const id = 1;
 
+	//hooks useCounter para pasar al siguiente pokemon
+	const { increment, counter } = useCounter(id);
+
 	//hooks useFetch para utilizar la api
 	const { data, isLoading, hasError } = useFetch(
-		`https://pokeapi.co/api/v2/pokemon/${id}`
+		`https://pokeapi.co/api/v2/pokemon/${counter}`
 	);
 
 	//funsion para obtener nombre
@@ -24,6 +28,13 @@ export const MultiplesCustomHooks = () => {
 	const { type } = !!Types && Types[0];
 	const { name } = !!type && type;
 	const nameType = name;
+
+	// const {
+	// 	other: {
+	// 		dream_world: { front_default: url },
+	// 	},
+	// } = !!data && data;
+	// const imgPokemon = url;
 
 	//condicion para ejecutar el isLoading mientras se obtiene la respuesta de la data
 	/* nota: de esta forma se utiliza si se desea establecer 
@@ -47,7 +58,13 @@ export const MultiplesCustomHooks = () => {
 				</blockquote>
 			)}
 
-			<button className='btn btn-primary'>Next Pokemon</button>
+			<button
+				className='btn btn-primary'
+				onClick={increment}
+				disabled={isLoading}
+			>
+				Next Pokemon
+			</button>
 		</>
 	);
 };
--- hook_app/src/hooks/useCounter.js
+++ hook_app/src/hooks/useCounter.js
@@ -10,10 +10,10 @@ export const useCounter = (initialValue = 10) => {
 		if (counter === 0) return;
 		setCounter(counter - 1);
 	};
-	const increment2 = (value = 1) => {
+	const increment2 = (value = 2) => {
 		setCounter(counter + value);
 	};
-	const decrement2 = (value = 1) => {
+	const decrement2 = (value = 2) => {
 		if (counter === 0) return;
 		setCounter(counter - value);
 	};
@@ -23,8 +23,8 @@ export const useCounter = (initialValue = 10) => {
 	return {
 		counter,
 		increment,
-		increment2,
 		decrement,
+		increment2,
 		decrement2,
 		reset,
 	};
