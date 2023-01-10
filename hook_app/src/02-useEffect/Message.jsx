cc657140929e9ad002bf438a3095d016ccc2c2c9
Eduardo <Epadrinoa@gmail.com>
2023-01-10 13:13:38 -0400

    use_effect_precauciones

--- hook_app/src/02-useEffect/Message.jsx
+++ hook_app/src/02-useEffect/Message.jsx
@@ -1,17 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Message = () => {
-	useEffect(() => {
-		console.log('se monto');
+	const [coords, setCoords] = useState({ x: 0, y: 0 });
 
+	useEffect(() => {
+		const onMouseMove = ({ x, y }) => {
+			setCoords({ x, y });
+		};
+
+		window.addEventListener('mousemove', onMouseMove);
 		return () => {
-			console.log('se desmonto');
+			window.removeEventListener('mousemove', onMouseMove);
 		};
 	}, []);
 
 	return (
 		<>
 			<h3>Usuario ya existe</h3>
+			{JSON.stringify(coords)}
 		</>
 	);
 };
