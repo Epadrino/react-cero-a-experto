d904c9cc1b997014921f16d86daaf369bce617de
Eduardo <Epadrinoa@gmail.com>
2023-01-18 18:06:17 -0400

    agrego_funcion_buscar
    se agrego la opción de búsqueda

--- seccion_catorce_HerosApp/src/heroes/pages/SearchPage.jsx
+++ seccion_catorce_HerosApp/src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,32 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
+
+import { useForm } from '../../hooks';
 import { HeroCard } from '../components/HeroCard';
 
 export const SearchPage = () => {
+	//constante para poder pasar parametros a la url
+	const navigate = useNavigate();
+
+	//constante para poder extraer parametros a la url
+	const location = useLocation();
+
+	//constante para poder extraer los query parametros del search a la eseLocalicacion
+	const { q = '' } = queryString.parse(location.search);
+
+	//custom Hook para uso del formulario
+	const { searchText, onInputChange } = useForm({
+		searchText: '',
+	});
+
+	//funcion para obtener el contenido del formulario y pasarlo a la url
+	const onSerchSubmit = (event) => {
+		event.preventDefault();
+		if (searchText.trim().length <= 1) return;
+
+		navigate(`?q=${searchText}`);
+	};
+
 	return (
 		<>
 			<h1>Search</h1>
@@ -10,13 +36,15 @@ export const SearchPage = () => {
 				<div className='col-5'>
 					<h4>Searching</h4>
 					<hr />
-					<form>
+					<form onSubmit={onSerchSubmit}>
 						<input
 							type='text'
 							placeholder='Search a hero'
 							className='form-control'
 							name='searchText'
 							autoComplete='off'
+							value={searchText}
+							onChange={onInputChange}
 						/>
 						<button className='btn btn-outline-primary mt-1'>
 							Search
@@ -30,10 +58,10 @@ export const SearchPage = () => {
 					<div className='alert alert-primary'>Search a hero</div>
 
 					<div className='alert alert-primary'>
-						No hero witch <b>ABC</b>
+						No hero witch <b>{q}</b>
 					</div>
 
-					<HeroCard />
+					{/* <HeroCard /> */}
 				</div>
 			</div>
 		</>
