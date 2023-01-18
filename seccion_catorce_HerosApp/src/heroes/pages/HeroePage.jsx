c5175f3485c19dcf3d4540bc77408a13d0ca830c
Eduardo <Epadrinoa@gmail.com>
2023-01-18 12:06:52 -0400

    use_memo
    se agrego el hook de useMemo para las funciones que requieren parámetros

--- seccion_catorce_HerosApp/src/heroes/pages/HeroePage.jsx
+++ seccion_catorce_HerosApp/src/heroes/pages/HeroePage.jsx
@@ -1,4 +1,5 @@
-import { Navigate, useParams } from 'react-router-dom';
+import { useMemo } from 'react';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { getHeroById } from '../helpers';
 
 export const HeroePage = () => {
@@ -6,16 +7,51 @@ export const HeroePage = () => {
 	const { id } = useParams();
 
 	//const para usar la funcion para obtener los parametros del heroe
-	const hero = getHeroById(id);
-	console.log(hero);
+	const hero = useMemo(() => getHeroById(id), [id]);
 
+	//const para navegar, usada en la funcion onReturn
+	const navigate = useNavigate();
+
+	//funcion para regresar a la pagina anterior
+	const onReturn = () => {
+		navigate(-1);
+	};
+
+	// condicion para redireccionar si el heroe no exite
 	if (!hero) {
 		return <Navigate to='/marvel' />;
 	}
 
 	return (
-		<>
-			<h1>{hero.superhero}</h1>
-		</>
+		<div className='row mt-5'>
+			<div className='col-4'>
+				<img
+					src={`/assets/heroes/${hero.id}.jpg`}
+					alt={hero.superhero}
+					className='img-thumbnail'
+				/>
+			</div>
+			<div className='col-8'>
+				<h3>{hero.superhero}</h3>
+				<ul className='list-group list-group-flush'>
+					<li className='list-group-item'>
+						<b>Alter ego:</b> {hero.alter_ego}
+					</li>
+					<li className='list-group-item'>
+						<b>Publisher:</b> {hero.publisher}
+					</li>
+					<li className='list-group-item'>
+						<b>First appearance:</b> {hero.first_appearance}
+					</li>
+				</ul>
+
+				<h5 className='mt-3'>Characters:</h5>
+				<p>{hero.characters}</p>
+
+				<button className='btn btn-outline-primary' onClick={onReturn}>
+					Back
+				</button>
+			</div>
+		</div>
 	);
 };
