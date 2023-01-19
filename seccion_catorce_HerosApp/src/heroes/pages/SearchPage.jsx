f0652fbdde08a5d1c2689bdb420d4e2f050d6c08
Eduardo <Epadrinoa@gmail.com>
2023-01-19 10:17:09 -0400

    optimizacion_de_busqueda
    -se completo la funcionalidad de la pagina de búsqueda
    -se optimizo la búsqueda

--- seccion_catorce_HerosApp/src/heroes/pages/SearchPage.jsx
+++ seccion_catorce_HerosApp/src/heroes/pages/SearchPage.jsx
@@ -4,6 +4,7 @@ import queryString from 'query-string';
 import { useForm } from '../../hooks';
 import { HeroCard } from '../components/HeroCard';
 import { getHeroesByName } from '../helpers';
+import { heroes } from '../data/heroes';
 
 export const SearchPage = () => {
 	//constante para poder pasar parametros a la url
@@ -23,10 +24,16 @@ export const SearchPage = () => {
 		searchText: q,
 	});
 
+	//constante para ocultar o visualizar el aviso de ninguna escritura
+	const showSearch = q.length === 0;
+
+	//constante para ocultar o visualizar el aviso de heroe no encontrado
+	const showError = q.length > 0 && heroes.length === 0;
+
 	//funcion para obtener el contenido del formulario y pasarlo a la url
 	const onSerchSubmit = (event) => {
 		event.preventDefault();
-		if (searchText.trim().length <= 1) return;
+		// if (searchText.trim().length <= 1) return;
 
 		navigate(`?q=${searchText}`);
 	};
@@ -58,10 +65,17 @@ export const SearchPage = () => {
 				<div className='col-7'>
 					<h4>Results</h4>
 					<hr />
+					<div
+						className='alert alert-primary animate__animated animate__fadeIn'
+						style={{ display: showSearch ? '' : 'none' }}
+					>
+						Search a hero
+					</div>
 
-					<div className='alert alert-primary'>Search a hero</div>
-
-					<div className='alert alert-primary'>
+					<div
+						className='alert alert-danger animate__animated animate__fadeIn'
+						style={{ display: showError ? '' : 'none' }}
+					>
 						No hero witch <b>{q}</b>
 					</div>
 					{heroes.map((hero) => (
