cc11ffc8a04c34ef860e0d0d9916d2fdd15dc238
Eduardo <Epadrinoa@gmail.com>
2023-01-25 10:31:31 -0400

    creacion_del_navbar
    -se creo el navbar de la aplicación

--- seccion_dieciseis_JournalApp/src/journal/layout/JournalLayout.jsx
+++ seccion_dieciseis_JournalApp/src/journal/layout/JournalLayout.jsx
@@ -1,10 +1,12 @@
 import { Box } from '@mui/system';
 import React from 'react';
+import { Navbar } from '../components/Navbar';
 
 export const JournalLayout = ({ children }) => {
+	const drawerWidth = 240;
 	return (
 		<Box sx={{ display: 'flex' }}>
-			{/* navbar */}
+			<Navbar drawerWidth={drawerWidth} />
 			{/* siderbar */}
 
 			<Box component='main' sx={{ flecGrow: 1, p: 3 }}>
